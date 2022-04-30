272f1b6aa1a0d7346005a28211c763870669e91e
oriquelme80 <oriquelme80@outlook.com>
2022-04-30 19:43:18 -0400

    Validacion de datos
    Falta revisar un error de post, pero se me acabo el tiempo

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getItems, getItem, createItem } = require("../controllers/tracks");
 const router = express.Router();
+const { validatorCreateItem } = require("../validators/tracks")
+const { getItems, getItem, createItem } = require("../controllers/tracks");
+
+
 
 router.get("/", getItems)
-router.post("/",createItem)
+router.post("/",validatorCreateItem,createItem)
 
 
 module.exports = router
