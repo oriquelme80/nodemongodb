0cc79a42a1af332319b32fd693962889fae4a518
Oscar Riquelme <oriquelme.github@gmail.com>
2022-05-16 19:50:23 -0400

    Controlador de archivos
    Clase Completa

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,29 +1,69 @@
-
+const fs = require ("fs");
+const { matchedData } = require("express-validator");
 const { storageModel } = require("../models");
+const {handleHttpError}= require("../utils/handleError");
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 /*CONECTAR*/
 const getItems = async (req,res) =>{
-    const data = await storageModel.find({});
-    res.send({data})
+    try{
+        const data = await storageModel.find({});
+        res.send({data})
+    }catch(e){
+        handleHttpError(res,'ERROR_GET_ITEMS');
+    }
+    
 };
 /*OBTENER REGISTROS*/
-const getItem = (req,res) =>{
+const getItem = async (req,res) =>{
+    try{
+        const {id}=matchedData(req);
+        const data = await storageModel.findById(id);
+        res.send({data});
+
+    }catch(e)
+    {
+        handleHttpError(res,'ERROR_DETALLE_ITEM');
+    }
 };
 /*CREAR REGISTROS*/
 const createItem = async (req,res) =>{
-    const {body,file} = req
-    console.log(file)
-    const fileData = {
-        filename: file.filename,
-        url:`${PUBLIC_URL}/${file.filename}`
-    }
-    const data = await storageModel.create(fileData)
-    res.send({data})
-};
-/*ACTUALIZAR REGISTROS*/
-const updateItem = (req,res) =>{};
-/*ELIMINAR REGISTROS*/
-const deleteItem = (req,res) =>{};
+    try{
+        const {file} = req
+        console.log(file)
+        const fileData = {
+            filename: file.filename,
+            url:`${PUBLIC_URL}/${file.filename}`
+            }
+        const data = await storageModel.create(fileData)
+        res.send({data})
 
-module.exports = {getItems,getItem,createItem,updateItem,deleteItem};
+    }catch(e){
+        handleHttpError(res,'ERROR_DELETE_ITEM');
+    }
+  
+};
+
+/*ELIMINAR REGISTROS*/
+const deleteItem = async (req,res) =>{
+    try{
+        const {id}=matchedData(req);
+        const dataFile = await storageModel.findById(id);
+        await storageModel.deleteOne({_id:id});
+        const {filename} = dataFile;
+        const filePath = `${MEDIA_PATH}/${filename}`; 
+        //fs.unlinkSync(filePath);
+        const data = {
+            filePath,
+            deleteItem:1
+        }
+        res.send({data});
+
+    }catch(e)
+    {
+        handleHttpError(res,'ERROR_DELETE_ITEM');
+    }
+};
+
+module.exports = {getItems,getItem,createItem,deleteItem};
--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const uploadMiddleware = require("../utils/handleStorage")
-const {createItem} = require("../controllers/storage")
+const {validatorGetItem} = require("../validators/storage");
+const {createItem,getItem,getItems,updateItem,deleteItem} = require("../controllers/storage")
 
+/*Lista de items*/
+router.get("/",getItems);
 
+/*Detalle de item*/
+router.get("/:id",validatorGetItem,getItem);
+
+/*Eliminar un item*/
+router.delete("/:id",validatorGetItem,deleteItem);
 
 router.post("/",uploadMiddleware.single("myfile"),createItem);
 
